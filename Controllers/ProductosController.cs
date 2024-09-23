e6a91c48ee5e724237d6ce216e9dd5c7f72f4816
RodrigoTLZ <201G0246@rcarbonifera.tecnm.mx>
2024-09-22 20:47:04 -0600

    Creacion del DTO de producto y el controller del mismo

---
+++ Controllers/ProductosController.cs
@@ -0,0 +1,19 @@
+﻿using ApiBRD.Models.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ApiBRD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductosController : ControllerBase
+    {
+        public ProductosController(LabsystePwaBrdContext context)
+        {
+            Context = context;
+        }
+
+        public LabsystePwaBrdContext Context { get; }
+    }
+}
