ec7916673643e5354b14cd493d79bd3e0c5a3a98
Daniel Andres Rodriguez Guerrero <201g0282@rcarbonifera.tecnm.mx>
2024-12-06 11:32:34 -0600

    Compresion de imagenes y resolucion al 80%

--- Helpers/ImagenConverter.cs
+++ Helpers/ImagenConverter.cs
@@ -4,6 +4,7 @@
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Formats;
 using SixLabors.ImageSharp.Formats.Webp;
+using SixLabors.ImageSharp.Processing;
 
 namespace ApiBRD.Helpers
 {
@@ -21,12 +22,21 @@ namespace ApiBRD.Helpers
             byte[] imageBytes = Convert.FromBase64String(base64String);
 
             string outputPath = $"{outputPathWithoutExtension}.webp";
-            string directory = Path.GetDirectoryName(outputPath)??throw new Exception("Error getting image directory");
+            string directory = Path.GetDirectoryName(outputPath) ?? throw new Exception("Error getting image directory");
             if (!Directory.Exists(directory))
             {
                 Directory.CreateDirectory(directory);
             }
             using var image = Image.Load(imageBytes);
+
+            int originalWidth = image.Width;
+            int originalHeight = image.Height;
+
+            int newWidth = (int)(originalWidth * 0.8);
+            int newHeight = (int)(originalHeight * 0.8);
+
+            image.Mutate(x => x.Resize(newWidth, newHeight));
+
             image.Save(outputPath, new WebpEncoder());
         }
     }
